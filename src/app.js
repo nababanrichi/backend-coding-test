642047f7303d05d97cc4b1749783406ce5a10aec
RP <richi.prathama@gmail.com>
2019-11-14 19:58:46 +0700

    add logs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-module.exports = (db) => {
+module.exports = (db, logger) => {
     /**
      * @api {get} /health Check App Health Status
      * @apiName checkHealth
@@ -81,6 +81,40 @@ module.exports = (db) => {
      *     }
      */
     app.post('/rides', jsonParser, (req, res) => {
+        // TODO: Dirty Code #2 - Need to find out how to set log globally
+        const requestStart = Date.now();
+
+        let body = req.body;
+
+        res.on("finish", () => {
+          const { rawHeaders, httpVersion, method, socket, url } = req;
+          const { remoteAddress, remoteFamily } = socket;
+          const { statusCode, statusMessage } = res;
+          const headers = res.getHeaders();
+      
+          if(statusCode !== 200){
+            console.log(
+              JSON.stringify({
+                timestamp: Date.now(),
+                processingTime: Date.now() - requestStart,
+                rawHeaders,
+                body,
+                httpVersion,
+                method,
+                remoteAddress,
+                remoteFamily,
+                url,
+                response: {
+                  statusCode,
+                  statusMessage,
+                  headers
+                }
+              })
+            );
+          }
+        });
+        // END: Dirty Code #2
+        
         const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
         const endLatitude = Number(req.body.end_lat);
@@ -90,35 +124,35 @@ module.exports = (db) => {
         const driverVehicle = req.body.driver_vehicle;
 
         if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
             });
         }
 
         if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
             });
         }
 
         if (typeof riderName !== 'string' || riderName.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
         }
 
         if (typeof driverName !== 'string' || driverName.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
         }
 
         if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
@@ -128,7 +162,7 @@ module.exports = (db) => {
         
         const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
                     message: 'Unknown error'
                 });
@@ -136,7 +170,7 @@ module.exports = (db) => {
 
             db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
                 if (err) {
-                    return res.send({
+                    return res.status(500).send({
                         error_code: 'SERVER_ERROR',
                         message: 'Unknown error'
                     });
@@ -188,16 +222,50 @@ module.exports = (db) => {
      *     }
      */
     app.get('/rides', (req, res) => {
+        // TODO: Dirty Code #2 - Need to find out how to set log globally
+        const requestStart = Date.now();
+
+        let body = req.body;
+
+        res.on("finish", () => {
+          const { rawHeaders, httpVersion, method, socket, url } = req;
+          const { remoteAddress, remoteFamily } = socket;
+          const { statusCode, statusMessage } = res;
+          const headers = res.getHeaders();
+      
+          if(statusCode !== 200){
+            console.log(
+              JSON.stringify({
+                timestamp: Date.now(),
+                processingTime: Date.now() - requestStart,
+                rawHeaders,
+                body,
+                httpVersion,
+                method,
+                remoteAddress,
+                remoteFamily,
+                url,
+                response: {
+                  statusCode,
+                  statusMessage,
+                  headers
+                }
+              })
+            );
+          }
+        });
+        // END: Dirty Code #2
+
         db.all('SELECT * FROM Rides', function (err, rows) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
                     message: 'Unknown error'
                 });
             }
 
             if (rows.length === 0) {
-                return res.send({
+                return res.status(404).json({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
@@ -248,16 +316,50 @@ module.exports = (db) => {
      *     }
      */
     app.get('/rides/:id', (req, res) => {
+        // TODO: Dirty Code #2 - Need to find out how to set log globally
+        const requestStart = Date.now();
+
+        let body = req.body;
+
+        res.on("finish", () => {
+          const { rawHeaders, httpVersion, method, socket, url } = req;
+          const { remoteAddress, remoteFamily } = socket;
+          const { statusCode, statusMessage } = res;
+          const headers = res.getHeaders();
+      
+          if(statusCode !== 200){
+            console.log(
+              JSON.stringify({
+                timestamp: Date.now(),
+                processingTime: Date.now() - requestStart,
+                rawHeaders,
+                body,
+                httpVersion,
+                method,
+                remoteAddress,
+                remoteFamily,
+                url,
+                response: {
+                  statusCode,
+                  statusMessage,
+                  headers
+                }
+              })
+            );
+          }
+        });
+        // END: Dirty Code #2
+
         db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
                     message: 'Unknown error'
                 });
             }
 
             if (rows.length === 0) {
-                return res.send({
+                return res.status(404).send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
